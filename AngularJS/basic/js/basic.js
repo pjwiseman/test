8f3296f7257132139408cb0cb6527fad0e5a050c
Peter Wiseman <peter.wiseman@gmail.com>
2015-01-26 00:11:21 +1000

    Adding scope

--- AngularJS/basic/js/basic.js
+++ AngularJS/basic/js/basic.js
@@ -1,32 +1,58 @@
-var myApp = angular.module('MyApp1', []);
+var app = angular.module('MyApp1', []);
 
-myApp.factory('Data', function() {
+app.factory('Data', function() {
 	return {word1: 'Crikey ', word2: 'Croc' };
 });
 
-myApp.controller('BasicController', ['$window', '$scope', 'Data', function($window, $scope, Data) {
+app.controller('BasicController', ['$window', '$scope', 'Data', function($window, $scope, Data) {
 	$scope.data = {name: Data.word1 + "Hello"};
 	$scope.click = function() { $window.alert($scope.data.name); window.alert('test2'); };
 	console.log("Created basic controller");
 }]);
 
-myApp.filter('world', ['Data', function(Data) {
+app.filter('world', ['Data', function(Data) {
 	return function(text) {
 		return text + [' World', Data.word2].join(' ');
 	}
 }]);
 
-myApp.directive("superman", function() {
+app.directive("superman", function() {
   return {
     restrict: "E",
     template: "<div> Here I am to save the day </div>"
   }
 });
 
-myApp.directive("enter", function(){
+app.directive("well", function(){
+ return function(scope, element) {
+	element.addClass('well');
+    }
+});
+
+app.directive("panel", function(){
+ return function(scope, element) {
+	element.addClass('panel');
+    }
+});
+
+app.directive("flag", function(){
+ return function(scope, element) {
+	element.addClass('flag');
+    }
+});
+
+app.directive("enter", function(){
  return function(scope, element) {
       element.bind("mouseenter", function(){
 		element.addClass('well');
       })
     }
+});
+
+app.directive("kid", function() {
+  return {
+    restrict: "E",
+	scope: {},
+    template: 'Kid: <input type="text" ng-model="chore"> {{chore}}'
+  };
 });
