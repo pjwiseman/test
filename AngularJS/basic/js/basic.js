8631da60a3af9495a2771d50587defcf7a946a9f
Peter Wiseman <peter.wiseman@gmail.com>
2015-01-25 12:02:47 +1000

    Added factory and directive

--- AngularJS/basic/js/basic.js
+++ AngularJS/basic/js/basic.js
@@ -1,13 +1,23 @@
 var myApp = angular.module('MyApp1', []);
 
-myApp.controller('BasicController', ['$window', '$scope', function($window, $scope) {
-	$scope.data = {name: "Hello"};
+myApp.factory('Data', function() {
+	return {word1: 'Crikey ', word2: 'Croc' };
+});
+
+myApp.controller('BasicController', ['$window', '$scope', 'Data', function($window, $scope, Data) {
+	$scope.data = {name: Data.word1 + "Hello"};
 	$scope.click = function() { $window.alert($scope.data.name); window.alert('test2'); };
 }]);
 
-myApp.filter('world', function() {
+myApp.filter('world', ['Data', function(Data) {
 	return function(text) {
-		return text + ' World';
+		return text + [' World', Data.word2].join(' ');
 	}
-});
+}]);
 
+myApp.directive("superman", function() {
+  return {
+    restrict: "E",
+    template: "<div> Here I am to save the day </div>"
+  }
+});
